53bf39f0c5606765335bc881a0c8be04daa26f26
Craig DAvignon <craig.davignon@gmail.com>
2018-05-14 15:13:00 -0400

    got core esthetics in for the home page.

--- showWatchList/urls.py
+++ showWatchList/urls.py
@@ -18,9 +18,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+import showsList.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
+    path('', views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
